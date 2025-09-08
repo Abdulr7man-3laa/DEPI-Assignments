14d028bfb118898f61f37d0f296a83f219d8809f
Boda263 <abdo.alaa.eldfy@gmail.com>
2025-09-08 18:14:02 +0300

    Day3 ORM

---
+++ Assignment 1 ORM/Project/Project.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Project
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
---
+++ Assignment 1 ORM/Project/ProjectConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Project
+{
+    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id)
+                .UseIdentityColumn(10, 10);
+
+            builder.Property(p => p.Name)
+                .HasColumnType("varchar(50)")
+                .HasDefaultValue("OurProject")
+                .IsRequired();
+
+            builder.Property(p => p.Cost)
+                .HasColumnType("money");
+
+            builder.HasCheckConstraint("CK_Project_Cost_Range", "Cost >= 500000 AND Cost <= 3500000");
+
+        }
+    }
+}
